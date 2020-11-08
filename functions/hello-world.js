03fddbc894bf96c2bf2cfe387bc95b66556a6f48
jlafer <jlafer@yahoo.com>
2020-11-07 19:02:34 -0800

    Update hello-world.js

--- functions/hello-world.js
+++ functions/hello-world.js
@@ -1,7 +1,7 @@
-exports.handler = function(context, event, callback) {
-    const result = {
-        message : "Hello Universe!"
-    }
+exports.handler = function (context, event, callback) {
+  const result = {
+    message: 'Hello Universe!'
+  }
 
-    callback(null, result);
-  };
+  callback(null, result);
+};
